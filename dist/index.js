0fefb06038ed0e5da170007486fee87d4e409bfd
linnjoha <linn.wallin@hotmail.com>
2024-01-02 09:37:07 +0100

    book info

--- dist/index.js
+++ dist/index.js
@@ -10,6 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 const booksEl = document.querySelectorAll(".book");
 const apiUrl = "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
 let books = [];
+const BookCoverEl = document.getElementById("bookCover");
+const infoBoxAboutEl = document.getElementById("bookInfo");
+const smallAbouts = document.getElementById("bookSmallAbouts");
+const infoBoxEl = document.querySelector(".book-container");
+const exitBookInfo = document.getElementById("exit");
+exitBookInfo === null || exitBookInfo === void 0 ? void 0 : exitBookInfo.addEventListener("click", () => {
+    infoBoxEl === null || infoBoxEl === void 0 ? void 0 : infoBoxEl.classList.remove("chosen-book");
+});
+//
 // fetch with apiUrl and saves the response as interface book
 const fetchBooks = () => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield fetch(apiUrl);
@@ -29,15 +38,16 @@ const addBooks = (books) => {
         bookEl.appendChild(bookTitle);
         bookEl.appendChild(bookAuthor);
         bookEl.addEventListener("click", () => {
+            infoBoxEl === null || infoBoxEl === void 0 ? void 0 : infoBoxEl.classList.add("chosen-book");
+            //first we remove all the children if there's any
+            clearChildren();
+            //function that shows and creates the infobox about the book
             createAboutBook(books, i);
         });
     });
 };
 const createAboutBook = (books, i) => {
     //creates the front of the book
-    const infoBoxEl = document.querySelector(".book-container");
-    infoBoxEl === null || infoBoxEl === void 0 ? void 0 : infoBoxEl.classList.add("chosen-book");
-    const BookCoverEl = document.getElementById("bookCover");
     const bookTitle = document.createElement("h2");
     bookTitle.innerText = books[i].title;
     bookTitle.classList.add("book-title");
@@ -46,25 +56,29 @@ const createAboutBook = (books, i) => {
     BookCoverEl === null || BookCoverEl === void 0 ? void 0 : BookCoverEl.appendChild(bookTitle);
     BookCoverEl === null || BookCoverEl === void 0 ? void 0 : BookCoverEl.appendChild(bookAuthor);
     // creates the info-pages
-    const infoBoxAboutEl = document.getElementById("bookInfo");
     const aboutTitle = document.createElement("h1");
     aboutTitle.classList.add("book-cover-title");
     aboutTitle.innerText = books[i].title;
     const aboutAuthor = document.createElement("h2");
     aboutAuthor.classList.add("book-cover-author");
     aboutAuthor.innerText = `by ${books[i].author}`;
+    aboutAuthor.classList.add("about-author");
     infoBoxAboutEl === null || infoBoxAboutEl === void 0 ? void 0 : infoBoxAboutEl.appendChild(aboutTitle);
     infoBoxAboutEl === null || infoBoxAboutEl === void 0 ? void 0 : infoBoxAboutEl.appendChild(aboutAuthor);
     // about
     const aboutBook = document.createElement("p");
     aboutBook.innerText = books[i].plot;
     infoBoxAboutEl === null || infoBoxAboutEl === void 0 ? void 0 : infoBoxAboutEl.appendChild(aboutBook);
-    const smallAbouts = document.getElementById("bookSmallAbouts");
     const smallAboutsInfo = document.createElement("p");
     smallAboutsInfo.innerText = `Audience: ${books[i].audience} 
- Pages:${books[i].pages}
- First Puplished : ${books[i].year} 
+ Pages: ${books[i].pages}
+ First Published: ${books[i].year} 
  `;
     smallAbouts === null || smallAbouts === void 0 ? void 0 : smallAbouts.appendChild(smallAboutsInfo);
+};
+const clearChildren = () => {
+    BookCoverEl === null || BookCoverEl === void 0 ? void 0 : BookCoverEl.replaceChildren();
+    infoBoxAboutEl === null || infoBoxAboutEl === void 0 ? void 0 : infoBoxAboutEl.replaceChildren();
+    smallAbouts === null || smallAbouts === void 0 ? void 0 : smallAbouts.replaceChildren();
 };
 export {};
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+//if null ???
 //
 import { book } from "./interface";
 const booksEl: NodeListOf<Element> = document.querySelectorAll(".book");
 const apiUrl: string =
   "https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books";
 let books: book[] = [];
-
+const BookCoverEl: HTMLElement | null = document.getElementById("bookCover");
+const infoBoxAboutEl: HTMLElement | null = document.getElementById("bookInfo");
+const smallAbouts: HTMLElement | null =
+  document.getElementById("bookSmallAbouts");
+const infoBoxEl = document.querySelector(".book-container");
+const exitBookInfo = document.getElementById("exit");
+exitBookInfo?.addEventListener("click", () => {
+  infoBoxEl?.classList.remove("chosen-book");
+});
+//
 // fetch with apiUrl and saves the response as interface book
 const fetchBooks = async () => {
   const response: Response = await fetch(apiUrl);
@@ -25,15 +35,16 @@ const addBooks = (books: book[]) => {
     bookEl.appendChild(bookTitle);
     bookEl.appendChild(bookAuthor);
     bookEl.addEventListener("click", () => {
+      infoBoxEl?.classList.add("chosen-book");
+      //first we remove all the children if there's any
+      clearChildren();
+      //function that shows and creates the infobox about the book
       createAboutBook(books, i);
     });
   });
 };
 const createAboutBook = (books: book[], i: number) => {
   //creates the front of the book
-  const infoBoxEl = document.querySelector(".book-container");
-  infoBoxEl?.classList.add("chosen-book");
-  const BookCoverEl: Element | null = document.getElementById("bookCover");
   const bookTitle: HTMLElement = document.createElement("h2");
   bookTitle.innerText = books[i].title;
   bookTitle.classList.add("book-title");
@@ -42,25 +53,29 @@ const createAboutBook = (books: book[], i: number) => {
   BookCoverEl?.appendChild(bookTitle);
   BookCoverEl?.appendChild(bookAuthor);
   // creates the info-pages
-  const infoBoxAboutEl: Element | null = document.getElementById("bookInfo");
   const aboutTitle: HTMLElement = document.createElement("h1");
   aboutTitle.classList.add("book-cover-title");
   aboutTitle.innerText = books[i].title;
   const aboutAuthor: HTMLElement = document.createElement("h2");
   aboutAuthor.classList.add("book-cover-author");
   aboutAuthor.innerText = `by ${books[i].author}`;
+  aboutAuthor.classList.add("about-author");
   infoBoxAboutEl?.appendChild(aboutTitle);
   infoBoxAboutEl?.appendChild(aboutAuthor);
   // about
   const aboutBook: HTMLElement = document.createElement("p");
   aboutBook.innerText = books[i].plot;
   infoBoxAboutEl?.appendChild(aboutBook);
-  const smallAbouts: HTMLElement | null =
-    document.getElementById("bookSmallAbouts");
   const smallAboutsInfo: HTMLElement = document.createElement("p");
   smallAboutsInfo.innerText = `Audience: ${books[i].audience} 
- Pages:${books[i].pages}
- First Puplished : ${books[i].year} 
+ Pages: ${books[i].pages}
+ First Published: ${books[i].year} 
  `;
   smallAbouts?.appendChild(smallAboutsInfo);
+};
+
+const clearChildren = () => {
+  BookCoverEl?.replaceChildren();
+  infoBoxAboutEl?.replaceChildren();
+  smallAbouts?.replaceChildren();
 };
